8c60ebc3566f7f7bdf4d6dfe645077066439b80a
wfanf5 <63809015+wfanf5@users.noreply.github.com>
2022-09-14 16:09:52 -0700

    Create Startlines.java

---
+++ HW/HW01/Exercise01/Startlines.java
@@ -0,0 +1,18 @@
+/** This is HW01: exercise 1a 
+ * Author: WFAN
+*/
+
+public class Starlines {
+    public static void main(String[] args){
+        String s = "*";
+        int i = 0;
+        while (i <= 4) {
+            System.out.println(s);
+            s += "*";
+            i += 1;
+        }
+        
+    }
+
+}
+
